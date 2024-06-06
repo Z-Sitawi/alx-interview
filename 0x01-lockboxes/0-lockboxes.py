8c1f7a8768f13b652fe8df45514b84eff16a899e
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-06 12:10:47 +0100

    solve v3

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,19 +3,18 @@
 
 
 def canUnlockAll(boxes):
-    # Number of boxes
-    nbr_boxes = len(boxes)
-    # Initialize a set to store keys possessed
-    keys_possessed = set(boxes[0])
-
-    # Iterate through boxes starting from index 1
-    for idx in range(1, nbr_boxes):
-        # Check if the box index is in the keys possessed
-        if idx in keys_possessed:
-            # Add all keys in the box to keys possessed
-            keys_possessed.update(boxes[idx])
-
-    if len(keys_possessed) >= nbr_boxes - 1:
-        return True
-    else:
-        return False
+    """
+        A method that determines if all the boxes can be opened
+    :param boxes: a list of lists
+    :return: true if all boxes can be opened, False otherwise.
+    """
+    keys = set()
+    keys.add(0)
+    locked_boxes = [0]
+    while locked_boxes:
+        box_to_open = locked_boxes.pop()
+        for key in boxes[box_to_open]:
+            if key not in keys:
+                keys.add(key)
+                locked_boxes.append(key)
+    return len(keys) == len(boxes)
