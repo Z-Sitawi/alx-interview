e32c5307c0cb19bb9c32794fb8237c8d899efeee
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-14 04:30:54 -0700

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,10 +6,10 @@ const request = require('request');
 // Make a GET request to the Star Wars API endpoint for the specified film ID
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, res, body) => {
   if (err) throw err; // Handle error if request fails
-  
+
   // Parse the response body (which is in JSON format) to extract the list of characters
   const actors = JSON.parse(body).characters;
-  
+
   // Call the function 'exactOrder' to process each character's URL
   exactOrder(actors, 0);
 });
@@ -22,12 +22,11 @@ const exactOrder = (actors, x) => {
   // Make a GET request to the URL of the character
   request(actors[x], function (err, res, body) {
     if (err) throw err; // Handle error if request fails
-    
+
     // Parse the response body (which is in JSON format) to extract the character's name
     console.log(JSON.parse(body).name); // Print the character's name to the console
-    
+
     // Recursively call exactOrder for the next character in the actors array
     exactOrder(actors, x + 1);
   });
 };
-
