33e635b0347e2b1eb34363b6a553c51058bd9a9b
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-25 13:41:44 +0100

    first solve

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+    Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """
+    :param coins: (list) representing a pile of coins of different values
+    :param total: (int) A given amount
+    :return: the fewest number of coins needed to meet a given amount total
+    """
+    if isinstance(coins, list):
+        coins = sorted(coins)
+        count = 0
+        while total > 0:
+            if total > max(coins):
+                total -= max(coins)
+                count += 1
+            else:
+                for i in range(len(coins)):
+                    if coins[i] > total and coins[i] != coins[0]:
+                        total -= coins[i-1]
+                        count += 1
+
+        if total < 0:
+            return -1
+        else:
+            return count
